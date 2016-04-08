4b2cad8a4afad9fa7683ccf6e349b22d04a491cb
Exapiper <exapiper@gmail.com>
2016-04-07 18:32:53 -0700

    lab1.sh completed first edition

---
+++ lab1/lab1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##Matthew Fleek 
+##Lab1
+
+echo "Enter a number"
+
+read "N1z"
+
+if (( "$N1z" <= 5 )) ; then
+
+	echo "Your number is less than 5 "
+else
+	echo "Your number is greater than 5"
+
+fi
+
+
