9abda6f6373b4b1cff8ea5345ed5bd1f4e843a7d
piper <exapiper@gmail.com>
2016-04-25 11:52:58 -0700

    lab6 initial commit

---
+++ lab6/lab6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Matthew Fleek 
+#CS225 Lab6 More Loops
+
+#Objective: Create a script called lab6.sh that will check to see if each media file in the medialab directory is in medialab/media.xml.
+
+#List the filenames in the medialab directory that are not in media.xml
+#Display the number of files in the medialab directory that are not in media.xml
+#List the media files by name in media.xml that do not exist in the medialab directory
+#Display the number of files that are in the media.xml that do not exist in the medialab directory
+
+#--------------------------------------------------------------------------------------
