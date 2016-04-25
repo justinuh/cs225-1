55e0a9f2b58be872958bb21f7dbaff88dc173d06
Matthew Fleek <exapiper@gmail.com>
2016-04-25 12:07:23 -0700

    make lab5.sh a bit nicer

--- lab5/lab5.sh
+++ lab5/lab5.sh
@@ -13,12 +13,20 @@
  
 
 #------------------------------------------------------------------------
-
+# change internal field separator to process by line only
 IFSOLD="$IFS"
 IFS=$'\n'
 
 mf1=$(cat ~/medialab/medialist.txt)
-rm foundfiles.txt lostfiles.txt
+
+if [[ -f foundfiles.txt ]]; then
+	rm foundfiles.txt
+fi
+
+if [[ -f lostfiles.txt ]]; then
+	rm lostfiles.txt
+fi  
+
 for count in $mf1; do
 	if [[ -f ~/medialab/$count ]]; then
 		echo "$count" >> foundfiles.txt 
@@ -29,4 +37,6 @@ done
 
 echo "$(cat foundfiles.txt | wc -l) files accounted for"
 echo "$(cat lostfiles.txt | wc -l) files not found"
+
+#set internal field separator back to normal 
 IFS=$IFSOLD
