418f3a0c21a3638745373084f5bba88a32dad3d4
Exapiper <exapiper@gmail.com>
2016-04-30 11:12:17 -0700

    lab7 create objective statement

---
+++ lab7/lab7.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Matthew Fleek
+#CS225 Lab7 -
+
+#Objective: Make a shell script that takes one argument
+#and duplicates the functionality of the basename command using pattern matching.
+
+#Desired Output:
+
+#lab7.sh /home/bob/test.txt 
+# "test.txt"
+
+
+
